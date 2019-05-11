6759efb7652446a65872483ca3ed8a5ee12315df
malexaims <malexaims@gmail.com>
2019-05-11 10:06:07 -0400

    Ex 2 HW04

--- 04/Wholemeal.hs
+++ 04/Wholemeal.hs
@@ -12,3 +12,22 @@ fun2' = sum
         . iterate (\n -> if even n
                          then n `div` 2
                          else 3*n+1)
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+treeInsert :: a -> Tree a -> Tree a
+treeInsert x Leaf = Node 0 Leaf x Leaf
+treeInsert x (Node _ treeL nodeName treeR)
+    | hL < hR   = Node (hR+1) (treeInsert x treeL) nodeName treeR
+    | otherwise = Node (hL+1) treeL nodeName (treeInsert x treeR)
+            where hL = height treeL
+                  hR = height treeR
+
+height :: Tree a -> Integer
+height Leaf = 0
+height (Node x _ _ _) = x
+
+foldTree :: [a] -> Tree a
+foldTree = foldr treeInsert Leaf
