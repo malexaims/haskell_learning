1dfea652e6385b879af289d2247b221dd21999fb
malexaims <malexaims@gmail.com>
2019-05-15 22:32:13 -0400

    Ex 6 HW5

--- 05/Calc.hs
+++ 05/Calc.hs
@@ -1,7 +1,10 @@
-{-# OPTIONS_GHC -Wall -Werror #-}
+-- {-# OPTIONS_GHC -Wall -Werror #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import ExprT as E
 import Parser as P
+import qualified Data.Maybe as Y
+import qualified Data.Map as M
 
 class Expr a where
   lit :: Integer -> a
@@ -41,6 +44,37 @@ instance Expr Mod7 where
   add (Mod7 x) (Mod7 y) = lit $ x+y
   mul (Mod7 x) (Mod7 y) = lit $ x*y
 
+class HasVars a where
+  var :: String -> a
+
+data VarExprT = Vlit Integer
+         | Vadd VarExprT VarExprT
+         | Vmul VarExprT VarExprT
+         | Vvar Integer
+  deriving (Show, Eq)
+
+instance Expr VarExprT where
+  lit x = Vlit x
+  add x y = Vadd x y
+  mul x y = Vmul x y
+
+instance HasVars VarExprT where
+  var x = Vvar (read x::Integer)
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+  var x = M.lookup x
+
+instance Expr (M.Map String Integer -> Maybe Integer) where
+  lit x = (\_ -> Just x)
+  add f g = \m -> case ((f m == Nothing) || (g m == Nothing)) of
+                    True -> Nothing
+                    False -> Just (Y.fromJust (f m) +
+                                   Y.fromJust (g m))
+  mul f g = \m -> case ((f m == Nothing) || (g m == Nothing)) of
+                    True -> Nothing
+                    False -> Just (Y.fromJust (f m) *
+                                   Y.fromJust (g m))
+
 reify :: ExprT -> ExprT
 reify = id
 
@@ -56,3 +90,8 @@ evalStr expr = case (P.parseExp E.Lit E.Add E.Mul expr) of
 
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
+
+withVars :: [(String, Integer)]
+          -> (M.Map String Integer -> Maybe Integer)
+          -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs
